633d3511938fefe2304b1222ddc302ff57652905
Abdelrahman <ayahia1@swarthmore.edu>
2022-07-22 11:32:24 +0200

    Adding the resizing test

---
+++ src/tests/api/images/resizeSpec.ts
@@ -0,0 +1,26 @@
+import resize from '../../../api/images/resize';
+import { promises as fs } from 'fs';
+import path from 'path';
+import fileExists from 'file-exists';
+const thumbnail_file = path.join(
+  __dirname,
+  '../../../api/images/imagesFolder',
+  'thumbnail/fjordx700x200.jpg'
+);
+
+describe('Checking if the Image resizing function works', async (): Promise<void> => {
+  beforeAll(async (): Promise<void> => {
+    if (await fileExists(thumbnail_file)) {
+      await fs.unlink(thumbnail_file);
+    }
+  });
+
+  it('Checking that the resizing function works', async (): Promise<void> => {
+    await resize('fjord', 700, 200, thumbnail_file);
+    expect(await fileExists(thumbnail_file)).toBe(true);
+  });
+
+  afterAll(async (): Promise<void> => {
+    await fs.unlink(thumbnail_file);
+  });
+});
